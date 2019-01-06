2c8bad1b9ba1f64a3553a220479b9f5450abc6ff
Alex Blavat <blavat.alex@gmail.com>
2019-01-06 16:30:12 -0500

    Feature/chapter11 12 (#9) :plane:
    * inheritance demo
    
    * inheritance

---
+++ js/chapter11_12.js
@@ -0,0 +1,67 @@
+window.onload = () => {
+    console.log("ready.");
+    //Symbols - unique always
+    let mysymbol = Symbol('test');
+    console.log(mysymbol);
+
+    //classes
+    class Vehicle {
+        constructor(make='', model=''){
+            this.make = make;
+            this.model = model;
+        }
+
+        setMake(newMake) {
+            this.make = newMake;
+        }
+
+        setModel(newModel) {
+            this.model = newModel;
+        }
+
+        /** getters **/
+        get make(){
+            return this._make;
+        }
+
+        get model(){
+            return this._model;
+        }
+
+        /** setters **/
+        set make(ma) {
+            this._make = ma;
+        }
+
+        set model(mo) {
+            this._model = mo;
+        }
+    }
+
+    class Truck extends Vehicle{
+        constructor(type = 'Truck', make, model){
+            super(make, model);
+            this.type = type;
+        }
+
+        get type(){
+            return this._type;
+        }
+
+        set type(ty){
+            this._type = ty;
+        }
+
+        //override toString
+        toString(){
+            return `Object you're looking at is a ${this.type}: ${this.model} and it's manufactured by ${this.make} `;
+        }
+    }
+
+
+    let x = new Truck(undefined, "Toyota", "Tacoma");
+
+
+    console.log(x.toString());
+    console.log(x.make + " " + x.model);
+};
