382a471e241aa6c9fb808d9b4e7975a8ecdcac24
Oleksiy Blavat <oleksiy.blavat@oath.com>
2019-01-05 21:12:47 -0500

    chapter 9 events capture and management done

--- js/chapter9.js
+++ js/chapter9.js
@@ -22,8 +22,43 @@ window.onload = () => {
     function logInteraction(e){
         console.log("Test bubbling up of the event, event detected in the outer most div holding the heading, and this is its target:");
         console.log(e.target);
+        console.log(e.toString());
     }
     holder.addEventListener("click", logInteraction);
 
+    const COLORS = {
+        0: 'green',
+        1: 'blue',
+        2: 'brown',
+        3: 'aquamarine',
+        4: 'aqua',
+        5: 'teal',
+        6: 'red',
+        7: 'palevioletred',
+        8: 'olive',
+        9: 'salmon',
+        10: 'navy'
+    };
 
-}
+    //keyboard events
+    let textbox = document.getElementById("textbox");
+    let mirror = document.getElementById("keyboardOutput"); //mirrors output from textbox
+
+    function keyPresses(e){
+        console.log(`Key Press Recorded -> Location: ${e.location} Key: ${e.key} ID: ${e.which}`);
+        if(e.which == 32) {
+            mirror.innerText += " ";
+        } else if ((e.which > 47 && e.which < 91) || (e.which > 183 && e.which < 193)){
+            mirror.innerText += e.key;
+        } else if (e.key == 'Backspace') {
+            mirror.innerText = mirror.innerText.slice(0, -1); // backspace
+        } else if (e.key == 'Escape'){
+            mirror.innerText = "";
+        }
+        let colorID = (Math.round(Math.random() * 10));
+        mirror.style.color = COLORS[colorID];
+    }
+
+    textbox.addEventListener("keydown", keyPresses, true);
+
+};
