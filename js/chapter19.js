1cfb49dbf7d26d08bd469989610cb4fd36ffd493
Oleksiy Blavat <oleksiy.blavat@verizonmedia.com>
2019-02-10 12:52:22 -0500

    Regex

--- js/chapter19.js
+++ js/chapter19.js
@@ -1,3 +1,24 @@
 window.onload = () => {
   console.log("ready");
+  let string = "A quick brown fox jumps over the lazy dog!"
+
+  let regex = /!/;
+  let find = regex.test(string); // Do we have a non word character - yes "!"
+  if (find) {
+    let match = regex.exec(string);
+    console.log(match);
+    console.log(match.length);
+    console.log(
+        `
+        First value: match[0] -> ${match[0]}
+        Second value: match[1] -> ${match[1]}
+        Key: index -> ${match["index"]}
+        Key: input -> ${match["input"]}
+        Key: groups -> ${match["groups"]} 
+        `
+    );
+  }
+  console.log(find);
+
+
 };
